a4256b188afe33083ad6cde7020bdf9e70ba230e
Adam Domanski <domaners@gmail.com>
2024-09-05 08:34:05 +0100

    Sprite maps, animations and walking enemies

--- enemy.gd
+++ enemy.gd
@@ -1,31 +1,11 @@
 extends Area2D
 
-@export var move_speed : float = 30.0
-@export var move_dir : Vector2 = Vector2()
 
-var start_position : Vector2 = Vector2()
-var target_position : Vector2 = Vector2(0, 0)
-#var global_position : Vector2 = Vector2()
-
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	start_position = global_position
-	target_position = start_position + move_dir
-	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	global_position = global_position.move_toward(target_position, move_speed * delta)
-	
-	if global_position == target_position:
-		if global_position == start_position:
-			target_position = start_position + move_dir
-		else:
-			target_position = start_position
-
-
-func _on_body_entered(body):
-	if body.is_in_group("Player"): 
-		print("HIT PLAYER")
-	elif body.is_in_group("PlayerSword"):
-		print("HIT ENEMY")
-	else:
-		print("WAAAAHHHHH")
-	
+	pass
