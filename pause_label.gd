855cccac439367c1aeb761fba38d2381e032de48
Adam Domanski <domaners@gmail.com>
2025-02-07 06:07:33 +0000

    Pause functionality
    Implemented a frame buffer for pausing to stop the game getting stuck on the initial load of level.

--- pause_label.gd
+++ pause_label.gd
@@ -1,13 +1,33 @@
 extends Label
 
+var frame_wait : float = 0.0 # Ensures the user cannot spam the pause button, this seems to get the player stuck sometimes...
+
+const PAUSE_BUFFER : float = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.visible = false # Replace with function body.
+	self.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	print("Current pause state: " + str(get_tree().paused))
+	print("Delta: " + str(delta))
+	
+	# Put some buffer in place so that the level doesn't begin paused. There must be a better way of handling this!!!
+	frame_wait = frame_wait + delta
+	if(frame_wait < PAUSE_BUFFER):
+		return
+	
 	if Input.is_action_just_pressed("BTN_START"):
-		self.visible = !get_tree().paused
-		get_tree().paused = !get_tree().paused
+		print("Previous pause state: " + str(get_tree().paused))
+		if(get_tree().paused == false):
+			self.visible = true
+			get_tree().set_deferred("paused", true)
+			frame_wait = 0
+		else:
+			self.visible = false
+			get_tree().set_deferred("paused", false)
+			frame_wait = 0
+		print("New pause state: " + str(get_tree().paused))
